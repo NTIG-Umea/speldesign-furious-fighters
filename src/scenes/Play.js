0e894645e35611b68916761cdeb2e4061c605d3e
pielun-nti <pierre.lundstrom@elev.ga.ntig.se>
2020-12-15 14:29:59 +0100

    refractor(Play): changed so play scene uses other files

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,4 +1,8 @@
 import Phaser from 'phaser';
+import CollectCoin from '../CollectCoin'
+import updateEnemies from '../Enemies'
+import updateEnemiesInRange from '../EnemiesInRange'
+import levelUpdater from '../LevelUpdater';
 
 export default class PlayScene extends Phaser.Scene {
   constructor () {
@@ -47,7 +51,7 @@ export default class PlayScene extends Phaser.Scene {
    this.groundLayer.setCollisionByExclusion([-1]);
 
    // coin image used as tileset
-   this.coinTiles = this.map.addTilesetImage('kaka');
+    this.coinTiles = this.map.addTilesetImage('kaka');
    // add coins as tiles
    this.coinLayer = this.map.createDynamicLayer('Coins', this.coinTiles, 0, 0);
 
@@ -83,7 +87,7 @@ for (var i = 0; i < 2; i++) {
     this.enemies.push(enemy);
 }
 
-   this.coinLayer.setTileIndexCallback(17, collectCoin, this);
+   this.coinLayer.setTileIndexCallback(17, CollectCoin, this);
    // when the this.player overlaps with a tile with index 17, collectCoin 
    // will be called    
    this.physics.add.overlap(this.player, this.coinLayer);
@@ -100,7 +104,6 @@ for (var i = 0; i < 2; i++) {
        frames: [{key: 'player', frame: 'p1_stand'}],
        frameRate: 10,
    });
-
    this.anims.create({
     key: 'spin', 
     frames: this.anims.generateFrameNames('kaka', { 
@@ -108,6 +111,7 @@ for (var i = 0; i < 2; i++) {
     frameRate: 10, 
     repeat: -1
 })
+
 
    this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -159,82 +163,39 @@ for (var i = 0; i < 2; i++) {
     if (this.scene.isVisible('pause')) {
         this.scene.setVisible(false, 'pause');
       }
-    if (this.coinLayer.culledTiles.length == 1) {
-        if (this.coins == undefined){
-        if (this.score >= 15 & this.score < 50){
-            this.level++;
-            this.textLevel.setText("Level: " + this.level);
-        }
-    }
-        if (this.coins != undefined){
-        if (this.coins.countActive(true) == 0){
-            if (this.score >= 50 & this.score < 100){
-                this.level++;
-                this.textLevel.setText("Level: " + this.level);
-            }
-            if (this.score >= 100 & this.score < 200){
-                this.level++;
-                this.textLevel.setText("Level: " + this.level)
-                //move player and level text a little bit
-                this.textPlayerHp = this.add.text(120, 570, 'Your HP: 100', {
-                    fontSize: '20px',
-                    fill: '#ffffff'
-                });
-                // fix the text to the camera
-                this.textPlayerHp.setScrollFactor(0);
-                this.textLevel = this.add.text(320, 570, 'Level: 1', {
-                    fontSize: '20px',
-                    fill: '#ffffff'
-                });
-                // fix the text to the camera
-                this.textLevel.setScrollFactor(0);
-            }
-            if (this.score >= 200 & this.score < 300){
-                this.level++;
-                this.textLevel.setText("Level: " + this.level);
-            }
-            if (this.score >= 300 & this.score < 400){
-                this.level++;
-                this.textLevel.setText("Level: " + this.level);
-            }
-            if (this.score >= 400 & this.score < 500){
-                this.level++;
-                this.textLevel.setText("Level: " + this.level);
-            }
-        }
-    }
+      if (this.coinLayer.culledTiles.length == 1) {
+        levelUpdater(this.coins, this.score, this.level, this.textLevel, this.textPlayerHp, this);
         if (this.coins == undefined) {
-        
-        this.coins = this.physics.add.group({
-    key: 'kaka',
-    repeat: 30,
-    setXY: { x: 0, y: 0, stepX: 70 }
-});
-
-this.physics.add.collider(this.groundLayer, this.coins);
-
-this.coins.children.iterate(function (child) {
-    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-});
-    this.physics.add.overlap(this.player, this.coins, function (player, coin) {
-        coin.disableBody(true, true);
-
-    this.score += 1;
-    this.textScore.setText('Score: ' + this.score);
-
-    if (this.coins.countActive(true) === 0)
-    {
-        this.coins.children.iterate(function (child) {
-
-            child.enableBody(true, child.x, 0, true, true);
-
-        });
-
-    }
-    }, null, this);
-}
-}
+          
+          this.coins = this.physics.add.group({
+      key: 'kaka',
+      repeat: 30,
+      setXY: { x: 0, y: 0, stepX: 70 }
+  });
+  this.physics.add.collider(this.groundLayer, this.coins);
+  
+  this.coins.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  
+  });
+      this.physics.add.overlap(this.player, this.coins, function (player, coin) {
+          coin.disableBody(true, true);
+  
+      this.score += 1;
+      this.textScore.setText('Score: ' + this.score);
+  
+      if (this.coins.countActive(true) === 0)
+      {
+          this.coins.children.iterate(function (child) {
+  
+              child.enableBody(true, child.x, 0, true, true);
+  
+          });
+  
+      }
+      }, null, this);
+        }
+      }
 if (this.level == 2){
     if (this.enemyCount < 5){
     for (var i = 0; i < 3; i++){
@@ -272,35 +233,8 @@ if (this.level == 3){
 }
 }
 this.enemiesInRange = [];
-    for (var enemy of this.enemies){
-        if (enemy.body.onFloor() & enemy.y <= 0) {
-            //enemy.y -= 10;
-            //enemy.setVelocityY(-500)
-        }
-        if (enemy.xPos >= this.playerX){
-            //enemy.xPos -= 10;
-            enemy.setVelocityX(-Phaser.Math.Between(100, 200), 20)
-            enemy.flipX = false;
-        }else if (enemy.xPos <= this.playerX) {
-            //enemy.xPos += 10;
-            enemy.setVelocityX(Phaser.Math.Between(100, 200), 20)
-            enemy.flipX = true;
-        }
-        var distanceToPlayerX = Math.abs(enemy.x - this.player.x);
-        var distanceToPlayerY = Math.abs(enemy.y - this.player.y);
-        var distance = Math.sqrt(Math.pow(distanceToPlayerX,2) + Math.pow(distanceToPlayerY, 2));
-        if (distance <= this.range) {
-            this.enemiesInRange.push(enemy);
-        }
-          
-    }
-    for (let enemy in this.enemiesInRange){
-        if (this.playerHp > 0 ){
-        this.playerHp -= 1;
-        this.textPlayerHp.setText("Your HP: " + this.playerHp); // set the text to show the current score   
-        this.enemiesInRange.splice(this.enemiesInRange.indexOf(enemy))
-        }
-    }
+    updateEnemies(this.player, this.enemies, this.playerX, this.range, this.enemiesInRange);
+    updateEnemiesInRange(this.enemiesInRange, this.playerHp, this.textPlayerHp);
     if (this.playerHp <= 0){
         this.scene.start('end');
         //this.scene.remove();
@@ -335,13 +269,5 @@ this.enemiesInRange = [];
 }
 }
 
-// this function will be called when the this.player touches a coin
-function collectCoin(sprite, tile) {
-    this.coinLayer.removeTileAt(tile.x, tile.y); // remove the tile/coin
-    //console.log(coinLayer.culledTiles.length)
-    this.score++; // add 1 points to the score
-    this.textScore.setText("Score: " + this.score); // set the text to show the current score
-    return false;
-}
-
+  
 

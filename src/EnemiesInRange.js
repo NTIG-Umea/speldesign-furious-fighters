85a45b3e3f4451b2fb1c1555b58cc78ab820440d
pielun-nti <pierre.lundstrom@elev.ga.ntig.se>
2020-12-17 09:43:23 +0100

    added K key for Kill, will continue doing it later

--- src/EnemiesInRange.js
+++ src/EnemiesInRange.js
@@ -1,6 +1,5 @@
 export default function(enemiesInRange, playerHp, textPlayerHp){
     for (let enemy in enemiesInRange){
-        console.log("ed: " + playerHp);
         if (playerHp > 0 ){
         playerHp -= 0.1;
         textPlayerHp.setText("Your HP: " + Math.round(playerHp)); // set the text to show the current score   

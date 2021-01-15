459fb0e79ef1efa7dd006c9c41903101ae122d8c
pielun-nti <pierre.lundstrom@elev.ga.ntig.se>
2021-01-15 08:52:10 +0100

    feat(Menu): added info about how you play the game

---
+++ src/Movement.js
@@ -0,0 +1,1 @@
+//cursor keys etc then move
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -16,6 +16,14 @@ export default class MenuScene extends Phaser.Scene {
     })
       .setOrigin(0.5, 0);
 
+      this.add.text(400, 400, 'The goal with this game is to survive and earn score.\n\nDifficulty increases each new level you reach.\n\nEnemies will hurt your hp if you come too close. Picking up hearts gives you score and health.', {
+        align: 'center',
+        fill: 'white',
+        fontFamily: 'sans-serif',
+        fontSize: 32
+      })
+        .setOrigin(0.5, 0);
+
     this.input.on('pointerdown', function () {
       this.scene.switch('play');
     }, this);
